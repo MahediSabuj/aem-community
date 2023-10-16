8b82a1c0706327a9d89f87011a36f07af5cb4106
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2023-10-17 03:13:52 +0600

    book image: new component

--- core/src/main/java/com/aem/community/core/components/internal/models/v1/BookItemImpl.java
+++ core/src/main/java/com/aem/community/core/components/internal/models/v1/BookItemImpl.java
@@ -2,61 +2,69 @@ package com.aem.community.core.components.internal.models.v1;
 
 import com.adobe.cq.export.json.ExporterConstants;
 import com.aem.community.core.components.models.BookImage;
-import com.day.cq.dam.api.Asset;
+import com.aem.community.core.components.models.BookItem;
 import com.drew.lang.annotations.Nullable;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.factory.ModelFactory;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Model(
   adaptables = SlingHttpServletRequest.class,
-  adapters = { BookImage.class },
-  resourceType = BookImageImpl.RESOURCE_TYPE,
+  adapters = { BookItem.class },
+  resourceType = BookItemImpl.RESOURCE_TYPE,
   defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(
   name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
   extensions = ExporterConstants.SLING_MODEL_EXTENSION)
-public class BookImageImpl implements BookImage {
-    public static final String RESOURCE_TYPE = "aem-community/components/book-image";
+public class BookItemImpl implements BookItem {
+    public static final String RESOURCE_TYPE = "aem-community/components/book-item";
 
-    private Asset imageAsset;
+    private BookImage bookImage;
 
-    @SlingObject
-    private ResourceResolver resourceResolver;
+    @Self
+    private SlingHttpServletRequest request;
+
+    @ScriptVariable
+    private Resource resource;
+
+    @Inject
+    private ModelFactory modelFactory;
 
     @ValueMapValue
     @Nullable
     @Default(values = "")
-    @Named("fileReference")
-    String fileReference;
+    @Named("title")
+    String bookTitle;
 
     @PostConstruct
     protected void init() {
-        if(StringUtils.isNotBlank(fileReference)) {
-            Resource assetResource = resourceResolver.getResource(fileReference);
-            if(assetResource != null) {
-                imageAsset = assetResource.adaptTo(Asset.class);
-            }
-        }
+        bookImage = modelFactory.getModelFromWrappedRequest(request, resource, BookImage.class);
     }
 
     @Override
     public String getSrc() {
-        return imageAsset == null ? StringUtils.EMPTY : imageAsset.getPath();
+        return bookImage == null ? StringUtils.EMPTY : bookImage.getSrc();
     }
 
     @Override
     public String getAlt() {
-        return imageAsset == null ? StringUtils.EMPTY : imageAsset.getMetadataValueFromJcr(IMAGE_TITLE);
+        return bookImage == null ? StringUtils.EMPTY : bookImage.getAlt();
+    }
+
+    @Override
+    public String getBookTitle() {
+        return bookTitle;
     }
 }
